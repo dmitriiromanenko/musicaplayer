a8c30baca6c47f675fbe0a133f9743410f2b53d2
dmitriiromanenko <dmitry.e.romanenko@gmail.com>
2021-11-04 21:28:49 +0100

    commit

---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Song = ({currentSong}) =>{
+    return (
+        <div className="song-container">
+            <img alt={currentSong.name} src={currentSong.cover}></img>
+            <h1>{currentSong.name}</h1>
+            <h1>{currentSong.artist}</h1>
+
+        </div>
+    )
+}
+
+export default Song;
---
+++ src/data.js
@@ -0,0 +1,62 @@
+import {v4 as uuidv4} from "uuid";
+
+function chillHop(){
+    return [
+        {
+            name :"Cabaña",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: true,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21647'
+        },
+        {
+            name :"Glaciar",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: false,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21648'
+        },
+        {
+            name :"What if I told you",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: false,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21649'
+        },
+        {
+            name :"Cabaña",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: true,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21647'
+        },
+        {
+            name :"Glaciar",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: false,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21648'
+        },
+        {
+            name :"What if I told you",
+            artist:"Juan Rios",
+            cover:"https://i.scdn.co/image/ab67616d0000b27336464bfa692b33ebc39730cd",
+            id: uuidv4(),
+            active: false,
+            color:['#BA4A46', '#FDF0DD'],
+            audio: 'https://mp3.chillhop.com/serve.php/?mp3=21649'
+        },
+    ]
+}
+
+export default chillHop;
